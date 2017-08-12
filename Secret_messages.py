1c8c869a0e2047a3faa2f8606e6a8b18212e8ae8
Denise Kahr <connectdeniz@gmail.com>
2017-08-11 19:56:15 -0700

    Update Secret_messages.py

--- Secret_messages.py
+++ Secret_messages.py
@@ -1,5 +1,49 @@
-from menu import Menu
+import os
+from affine import Affine
+from atbash import Atbash
+from polybius import Polybius
+
+"""The first screen shows all avalaible options to encrypt and decrypt text."""
+def cipher_selection():
+    selection = input("Encrypt or decrypt via cipher?\n"
+                               "Via Affine: To encrypt, enter AFE. To decrypt, enter AFD.\n"
+                               "Via Atbash: To encrypt, enter ABE. To decrypt, enter ABD.\n"
+                               "Via Polybius: To encrypt, enter POE. To decrypt, enter POD.\n")
+    """Based on the selection and user input(text), associated cipher script will run."""
+    if selection == "AFE":
+        alpha = input('Enter an alpha. Available values: 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25')
+        beta = input('Enter a beta value.')
+        text = input('What is your message?\n')
+        x = Affine(alpha,beta)
+        x.encrypt(text)
+    if selection == "AFD":
+        alpha = input('Enter an alpha. Available values: 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25\n')
+        beta = input('Enter a beta value.')
+        text = input('What is your message?\n')
+        Affine(alpha, beta)
+    if selection == 'ABE':
+        self.text = input('What is your message?\n')
+        x = Atbash()
+        text = input('What is your message?\n')
+        return x.encrypt(text)
+    if selection == 'ABD':
+        x = Atbash()
+        text = input('What is your message?\n')
+        return x.decrypt(text)
+    if selection == ' POE':
+        x = Polybius()
+        text = input('What is your message?\n')
+        return x.encrypt(text)
+    if selection == 'POD':
+        x = Polybius()
+        text = input('What is your message?\n')
+        return x.decrypt(text)
+    else:
+        return "Your selection is not valid. Please try again."
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == '__main__':
-    print("test block: hello")
-    Menu()
+    cipher_selection()
+    clear_screen()
